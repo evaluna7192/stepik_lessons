a0bd664026b227c848d0bf5901bd2c7bbc6f40dd
Дарья Гимпель <darya.gimpel@interfax.ru>
2021-02-10 20:21:28 +0300

    add registration locator_samples test_registration files to module_2

---
+++ module_2/test_registration.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from sys import argv
+import time
+
+script_name, link = argv
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_css_selector("input.first[required]")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector("input.second[required]")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector("input.third[required]")
+    input3.send_keys("test@gmail.com")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
