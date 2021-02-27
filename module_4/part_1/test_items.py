5b2ee7d62eaf9871c991edb7af531fa14e64f27b
Дарья Гимпель <darya.gimpel@interfax.ru>
2021-02-27 21:42:49 +0300

    update test_sample file - delete time sleep and add additional locator

---
+++ module_4/part_1/test_items.py
@@ -0,0 +1,13 @@
+import time
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+add_to_basket_button_locator = ".btn-add-to-basket"
+
+
+def test_add_to_basket_button_is_presented(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+    time.sleep(5)
+
+    add_to_basket_button = browser.find_element_by_css_selector(add_to_basket_button_locator)
+    assert add_to_basket_button, \
+        "Page does not have an add to basket button"
